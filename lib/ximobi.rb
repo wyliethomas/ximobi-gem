c1b61b9c9345cfa7b36e25363133eaf7a5fa10b0
Wylie Thomas <wyliethomas@Wylies-MacBook-Pro.local>
2014-03-05 21:43:57 -0700

    added expense

--- lib/ximobi.rb
+++ lib/ximobi.rb
@@ -4,9 +4,11 @@ require "uri"
 
 module Ximobi
   class Entry
+    #YEAH YEAH, ITS A LOT OF DUPE BUT IM THE ONLY USER AND IM IN A HURRY SO DEAL WITH IT
     def time
       case ARGV[0].downcase
       when "time"
+        #TIME
         print "Email: "
         email = $stdin.gets.chomp
         print "Categories: "
@@ -26,12 +28,38 @@ module Ximobi
                                "categories" => categories,
                                "time" => "#{time}",
                                "description" => "#{description}",
+                               "type" => "time"
                               })
 
         response = http.request(request)
+        puts Time.now
 
       when "expense"
-        print "Expense entries not yet available for command line use \n"
+        #EXPENSE
+        print "Email: "
+        email = $stdin.gets.chomp
+        print "Categories: "
+        categories = $stdin.gets.chomp
+        #categories = categories.split(",")
+        print "Amount: "
+        amount = $stdin.gets.chomp
+        print "Description: "
+        description = $stdin.gets.chomp
+        
+        #Post the data to the webservice
+        uri = URI.parse("http://ximobi.wyliethomas.com/capture/gem")
+        http = Net::HTTP.new(uri.host, uri.port)
+
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.set_form_data({"email" => "#{email}", 
+                               "categories" => categories,
+                               "amount" => "#{amount}",
+                               "description" => "#{description}",
+                               "type" => "expense"
+                              })
+
+        response = http.request(request)
+        puts Time.now
       end
       
     end
